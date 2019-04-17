4fb13fd1763ef04de53c929afdbe5f95c7424dfe
Keirron Stach <stachkeirron@gmail.com>
2019-04-17 17:27:40 +0930

    Added EventSystem source manager
    Event System does the logic behind Event Manager and Local Event Manager

---
+++ Assets/Plugins/KestCast/EventManager/EventSystem.cs
@@ -0,0 +1,135 @@
+﻿// EventSystem.CS
+// Author: Chris Nyman / Keirron Stach
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+namespace KestCast.EventManager
+{
+    /// <summary>
+    /// Represent and event system
+    /// </summary>
+    public class EventSystem : MonoBehaviour
+    {
+        private Dictionary<string, UnityEvent> eventDictionary;
+        private Dictionary<Type, object> eventDictionaryType;
+
+        public EventSystem()
+        {
+            eventDictionary = new Dictionary<string, UnityEvent>();
+            eventDictionaryType = new Dictionary<Type, object>();
+        }
+
+        public void StartListening(string eventName, UnityAction listener)
+        {
+            UnityEvent thisEvent = null;
+            if (eventDictionary.TryGetValue(eventName, out thisEvent))
+            {
+                thisEvent.AddListener(listener);
+            }
+            else
+            {
+                thisEvent = new UnityEvent();
+                thisEvent.AddListener(listener);
+                eventDictionary.Add(eventName, thisEvent);
+            }
+        }
+
+        public void StopListening(string eventName, UnityAction listener)
+        {
+            UnityEvent thisEvent = null;
+            if (eventDictionary.TryGetValue(eventName, out thisEvent))
+            {
+                thisEvent.RemoveListener(listener);
+            }
+        }
+
+        public void TriggerEvent(string eventName)
+        {
+            UnityEvent thisEvent = null;
+            if (eventDictionary.TryGetValue(eventName, out thisEvent))
+            {
+                thisEvent.Invoke();
+            }
+        }
+
+        public void StartListening<T>(string eventName, UnityAction<T> listener)
+        {
+            Dictionary<string, GenericEvent<T>> typeDictionary = null;
+            object baseDict = null;
+
+            if(eventDictionaryType.TryGetValue(typeof(T), out baseDict))
+            {
+                typeDictionary = baseDict as Dictionary<string, GenericEvent<T>>;
+            }
+            else
+            {
+                typeDictionary = new Dictionary<string, GenericEvent<T>>();
+                eventDictionaryType.Add(typeof(T), typeDictionary);
+            }
+
+            GenericEvent<T> thisEvent = null;
+
+            if (typeDictionary.TryGetValue(eventName, out thisEvent))
+            {
+                thisEvent.AddListener(listener);
+            }
+            else
+            {
+                thisEvent = new GenericEvent<T>();
+                thisEvent.AddListener(listener);
+                typeDictionary.Add(eventName, thisEvent);
+            }
+        }
+
+        public void StopListening<T>(string eventName, UnityAction<T> listener)
+        {
+            Dictionary<string, GenericEvent<T>> typeDictionary = null;
+            object baseDict = null;
+
+            if (eventDictionaryType.TryGetValue(typeof(T), out baseDict))
+            {
+                typeDictionary = baseDict as Dictionary<string, GenericEvent<T>>;
+            }
+            else
+            {
+                return;
+            }
+
+            GenericEvent<T> thisEvent = null;
+            if (typeDictionary.TryGetValue(eventName, out thisEvent))
+            {
+                thisEvent.RemoveListener(listener);
+            }
+        }
+
+        public void TriggerEvent<T>(string eventName, T param)
+        {
+            Dictionary<string, GenericEvent<T>> typeDictionary = null;
+            object baseDict = null;
+
+            if (eventDictionaryType.TryGetValue(typeof(T), out baseDict))
+            {
+                typeDictionary = baseDict as Dictionary<string, GenericEvent<T>>;
+            }
+            else
+            {
+                return;
+            }
+
+            GenericEvent<T> thisEvent = null;
+            if (typeDictionary.TryGetValue(eventName, out thisEvent))
+            {
+                thisEvent.Invoke(param);
+            }
+        }
+
+        private class GenericEvent<T> : UnityEvent<T>
+        {
+
+        }
+    }
+}
+
---
+++ Assets/Plugins/KestCast/EventManager/LocalEventManager.cs
@@ -0,0 +1,192 @@
+﻿// LocalEventManager.cs
+// Author: Keirron Stach
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace KestCast.EventManager
+{
+    /// <summary>
+    /// An event manager to handle event locally to one gameobject
+    /// </summary>
+    public class LocalEventManager : MonoBehaviour
+    {
+        //Our event dictionary
+        private EventSystem eventSystem;
+
+        public LocalEventManager()
+        {
+            eventSystem = new EventSystem();
+        }
+
+        /// <summary>
+        /// Start listening to an event trigger
+        /// </summary>
+        /// <param name="gameObject">The gameobject calling the event manager.</param>
+        /// <param name="eventName">Name of the event to listen too.</param>
+        /// <param name="listener">The listener that will be registered. This will be called every time the event fires.</param>
+        public static void StartListening(GameObject gameObject, string eventName, UnityAction listener)
+        {
+            if (gameObject == null)
+            {
+                Debug.LogWarning("The game object passed in was null.");
+                return;
+            }
+            var localEventManager = gameObject.GetComponent<LocalEventManager>();
+
+            if (localEventManager == null)
+            {
+                localEventManager = gameObject.AddComponent<LocalEventManager>();
+            }
+
+            localEventManager.StartListening(eventName, listener);
+        }
+
+        public void StartListening(string eventName, UnityAction listener)
+        {
+            eventSystem.StartListening(eventName, listener);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="behaviour"></param>
+        /// <param name="eventName"></param>
+        /// <param name="listener"></param>
+        public static void StopListening(GameObject gameObject, string eventName, UnityAction listener)
+        {
+            if (gameObject == null)
+            {
+                Debug.LogWarning("The game object passed in was null.");
+                return;
+            }
+            var localEventManager = gameObject.GetComponent<LocalEventManager>();
+
+            if (localEventManager == null)
+            {
+                Debug.LogWarning("No local event manager was found on " + gameObject.name + ".");
+                return;
+            }
+
+            localEventManager.StopListening(eventName, listener);
+        }
+
+        public void StopListening(string eventName, UnityAction listener)
+        {
+            eventSystem.StopListening(eventName, listener);
+        }
+
+        /// <summary>
+        /// Trigger an event
+        /// </summary>
+        /// <param name="gameObject">The game object on which the event will occur</param>
+        /// <param name="eventName">The event identifier</param>
+        public static void TriggerEvent(GameObject gameObject, string eventName)
+        {
+            if (gameObject == null)
+            {
+                Debug.LogWarning("The game object passed in was null.");
+                return;
+            }
+            var localEventManager = gameObject.GetComponent<LocalEventManager>();
+
+            if (localEventManager == null)
+            {
+                Debug.LogWarning("No local event manager was found on " + gameObject.name + ".");
+                return;
+            }
+
+            localEventManager.TriggerEvent(eventName);
+        }
+
+        public void TriggerEvent(string eventName)
+        {
+            eventSystem.TriggerEvent(eventName);
+        }
+
+        /// <summary>
+        /// Start listening to an event trigger with a specified type
+        /// </summary>
+        /// <param name="gameObject">The gameobject calling the event manager.</param>
+        /// <param name="eventName">Name of the event to listen too.</param>
+        /// <param name="listener">The listener that will be registered. This will be called every time the event fires.</param>
+        public static void StartListening<T>(GameObject gameObject, string eventName, UnityAction<T> listener)
+        {
+            if (gameObject == null)
+            {
+                Debug.LogWarning("The game object passed in was null.");
+                return;
+            }
+            var localEventManager = gameObject.GetComponent<LocalEventManager>();
+
+            if (localEventManager == null)
+            {
+                localEventManager = gameObject.AddComponent<LocalEventManager>();
+            }
+
+            localEventManager.StartListening(eventName, listener);
+        }
+
+        public void StartListening<T>(string eventName, UnityAction<T> listener)
+        {
+            eventSystem.StartListening(eventName, listener);
+        }
+
+        /// <summary>
+        /// Stop listening to an event with a specified type.
+        /// </summary>
+        /// <param name="behaviour"></param>
+        /// <param name="eventName"></param>
+        /// <param name="listener"></param>
+        public static void StopListening<T>(GameObject gameObject, string eventName, UnityAction<T> listener)
+        {
+            if (gameObject == null)
+            {
+                Debug.LogWarning("The game object passed in was null.");
+                return;
+            }
+            var localEventManager = gameObject.GetComponent<LocalEventManager>();
+
+            if (localEventManager == null)
+            {
+                Debug.LogWarning("No local event manager was found on " + gameObject.name + ".");
+                return;
+            }
+
+            localEventManager.StopListening(eventName, listener);
+        }
+
+        public void StopListening<T>(string eventName, UnityAction<T> listener)
+        {
+            eventSystem.StopListening(eventName, listener);
+        }
+
+        /// <summary>
+        /// Trigger an event
+        /// </summary>
+        /// <param name="gameObject">The game object on which the event will occur</param>
+        /// <param name="eventName">The event identifier</param>
+        public static void TriggerEvent<T>(GameObject gameObject, string eventName, T param)
+        {
+            if (gameObject == null)
+            {
+                Debug.LogWarning("The game object passed in was null.");
+                return;
+            }
+            var localEventManager = gameObject.GetComponent<LocalEventManager>();
+
+            if (localEventManager == null)
+            {
+                Debug.LogWarning("No local event manager was found on " + gameObject.name + ".");
+                return;
+            }
+
+            localEventManager.TriggerEvent(eventName, param);
+        }
+
+        public void TriggerEvent<T>(string eventName, T param)
+        {
+            eventSystem.TriggerEvent(eventName, param);
+        }
+    }
+}
+
